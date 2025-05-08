e9c8f020bf8b03620c1ab3991e3a4f68fbcdc43c
Yashraj8959 <ykushwah7828@gmail.com>
2025-05-08 18:14:09 +0530

    Add initial HTML and JavaScript files for interview practice

---
+++ INTERVIEW/practice.js
@@ -0,0 +1,62 @@
+// let a=10;
+// function print(){
+//     let a=4
+//     console.log(a)
+
+// }
+// print()
+// console.log(a)
+
+// const arr = [1,2,3,4,5];
+// console.log(arr)
+// const newarr = arr.map((elem, i, array) => {
+//     // array[i]=elem+1;
+//     // console.log(array)
+//     return array[i]*2
+// });
+// console.log(newarr);
+
+// const arrfn = ( )=> {
+//     console.log(this)
+// }
+
+// arrfn()
+
+// arrfn.apply(null, [1,2,3])
+
+// const fetch = require('node-fetch');
+// const databreach = async ()=>{
+//     const url = 'https://email-breach-search.p.rapidapi.com/rapidapi/search-email/ykushwah7828@gmail.com';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '0b6f4ccc6bmsh61d2fad53985a1bp1ed312jsnc235a8c6b87b',
+// 		'x-rapidapi-host': 'email-breach-search.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+// }
+
+// databreach()
+// array=[1,2,3,4,5]
+
+// array.forEach(element => {
+// 	console.log(element);
+// 	return;
+// 	console.log("hee")
+
+// 	console.log('helo')
+// });
+
+// array.every()
+var s= "dsfsfkj"
+console.log(s.slice(2))
+console.log(s)
---
+++ INTERVIEW/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+array=[1,2,3,4,5]
+
+// array.forEach(element => {
+// 	console.log(element);
+// 	return;
+// 	console.log("hee")
+
+// 	console.log('helo')
+// });
